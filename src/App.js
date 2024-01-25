af836149095a77888f980c42920baf2e8023ea49
Tufail Dar <tufaildar2016@gmail.com>
2024-01-25 13:50:48 +0530

    Add Expense List and Footer components

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "./index.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Header />
       <ExpenseForm />
+      <ExpenseList />
+      <Footer />
     </div>
   );
 }
@@ -22,6 +24,7 @@ function Header() {
 function ExpenseForm() {
   return (
     <div className="expense-form">
+      <h3>Add Expense</h3>
       <form>
         <label>Description</label>
         <input type="text" required />
@@ -32,5 +35,45 @@ function ExpenseForm() {
         <button>Submit</button>
       </form>
     </div>
+  );
+}
+
+function ExpenseList() {
+  return (
+    <div className="expense-list">
+      <div className="expense-list-info">
+        <h2>Expense List</h2>
+        <h4>Total Amount Spent : $XXX</h4>
+      </div>
+      <div className="expense-list-container">
+        <ExpenseItem />
+        <ExpenseItem />
+        <ExpenseItem />
+        <ExpenseItem />
+        <ExpenseItem />
+      </div>
+    </div>
+  );
+}
+
+function ExpenseItem() {
+  return (
+    <div className="expense-item">
+      <div className="expense-item__info">
+        <h4>Groceries</h4>
+        <p>Amount: $200</p>
+        <p>Date: 2022-01-01</p>
+      </div>
+      <button className="expense-item__delete">Delete</button>
+    </div>
+  );
+}
+function Footer() {
+  return (
+    <footer
+      style={{ textAlign: "center", padding: "20px", background: "#f8f9fa" }}
+    >
+      Crafted with ❤️ by Tufail Dar
+    </footer>
   );
 }
